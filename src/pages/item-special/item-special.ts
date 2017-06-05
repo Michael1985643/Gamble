d0fc7ea1982b0f1528ad68a4350580d0237337af
Michael1985643 <msteenkist@gmail.com>
2017-06-05 17:30:31 +0200

    special validation

--- src/pages/item-special/item-special.ts
+++ src/pages/item-special/item-special.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
 import { AngularFire } from 'angularfire2';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ItemSpecial page.
@@ -18,33 +19,87 @@ export class ItemSpecial {
   af;
   user;
   item;
-  homeGoals: number;
-  awayGoals: number;
+  homeGoals: any;
+  awayGoals: any;
+  choosenSpecials: any;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     af: AngularFire,
-    private _auth: AuthService) {
+    private _auth: AuthService,
+    public alertCtrl: AlertController) {
      this.af = af;
      this.user = _auth.auth$.getAuth().auth;
-     this.item = this.navParams.get('item');
+     this.item = this.navParams.get('item'); 
+        
+     this.af.database.list('/dnb/players/specials/' +  this.item.id).subscribe(result => {
+        this.choosenSpecials = result;
+     });       
+     this.getSpecial();
+  }
+
+  getSpecial() {
+    const firebaseObservable = this.af.database.object('/dnb/players/specials/' +  this.item.id + '/' + this.user.uid).forEach(itemArray => {
+      this.homeGoals = itemArray.homeGoals;
+      this.awayGoals = itemArray.awayGoals;
+    })
   }
 
   setSpecial() {
-    const path = this.af.database.object('/dnb/players/' + this.user.uid + '/' + this.item.id);
+    if (!this.isValid()) { return; };
+    const path = this.af.database.object('/dnb/players/specials/' +  this.item.id + '/' + this.user.uid);
     let newItem = {  
-        id: this.item.id,
-        homeGoals: 4,
-        awayGoals: this.awayGoals
+        homeGoals: parseInt(this.homeGoals) >= 0 ? parseInt(this.homeGoals) : "",
+        awayGoals: parseInt(this.awayGoals) >= 0 ? parseInt(this.awayGoals) : ""
     }
-
     path.set(newItem); 
-
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ItemSpecial');
+  isValid() {
+    if (this.homeGoals >= 0 && this.awayGoals >= 0) {
+      var valid = true;
+      this.choosenSpecials.forEach(specialUser => {
+        if (specialUser.homeGoals == this.homeGoals && specialUser.awayGoals == this.awayGoals && specialUser.$key != this.user.uid ) {
+          valid = false;
+          return valid;
+        }
+      });
+      if (!valid) {
+        this.showSpecials();
+      }
+      else {
+        return valid;
+      }
+    } 
+    else {
+      return false;
+    }
   }
+
+  showSpecials() {
+    var alertText= "<ul>";
+    this.choosenSpecials.forEach(specialUser => {
+      if (specialUser.$key != this.user.uid) {
+        alertText += "<li>" + specialUser.homeGoals + " - " + specialUser.awayGoals + "</li>";
+      }
+    })
+    alertText += "</ul>";
+
+    let alert = this.alertCtrl.create({
+      title: 'Gekozen specials',
+      subTitle: alertText,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.homeGoals = "";
+            this.awayGoals = "";
+          }
+        }
+      ]
+    });
+    alert.present();
+  }  
 
 }
